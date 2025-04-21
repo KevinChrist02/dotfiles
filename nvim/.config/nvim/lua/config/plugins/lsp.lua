a39dcf8ae42dd592583475f70452f78f68608cf6
whoiskevin <christkevin50@gmail.com>
2025-04-21 08:37:48 +0200

    added my nvim config

---
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -0,0 +1,54 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            'saghen/blink.cmp',
+            {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
+        config = function()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+            require("lspconfig").lua_ls.setup { capabilities = capabilities }
+            require('lspconfig').ruby_lsp.setup { capabilities = capabilities }
+            require('lspconfig').gopls.setup { capabilities = capabilities }
+            require('lspconfig').html.setup { capabilities = capabilities }
+            require('lspconfig').cssls.setup { capabilities = capabilities }
+            require('lspconfig').tailwindcss.setup { capabilities = capabilities }
+            require('lspconfig').emmet_language_server.setup { capabilities = capabilities }
+            require 'lspconfig'.pylsp.setup { capabilities = capabilities }
+            require 'lspconfig'.ruff.setup { capabilities = capabilities }
+
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if not client then return end
+
+                    if client.supports_method('textDocument/formatting') then
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                            end,
+                        })
+                    end
+                end,
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+        end,
+    }
+}
