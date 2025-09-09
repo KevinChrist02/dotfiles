676650831fd77472d2ac684116e770c3de58612b
Kevin Christ <christkevin50@gmail.com>
2025-09-09 11:34:02 +0200

    changed the colorscheme

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+    "EdenEast/nightfox.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("gruvbox")
+		vim.cmd.colorscheme("carbonfox")
 	end,
 }
