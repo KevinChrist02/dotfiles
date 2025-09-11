a5271064b5ca1e17046e973579e23745e60007d5
Kevin Christ <christkevin50@gmail.com>
2025-09-11 17:08:31 +0200

    updated blink

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,21 +1,23 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  opts = {
-    keymap = { preset = 'super-tab' },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+	opts = {
+		keymap = { preset = "default" },
 
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    completion = { documentation = { auto_show = false } },
+		completion = { documentation = { auto_show = false } },
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+
+		signature = { enable = true },
+	},
+	opts_extend = { "sources.default" },
 }
