f83641101de222ebe4b07744cefe19c881fe8c8f
Kevin Christ <christkevin50@gmail.com>
2025-09-11 14:57:53 +0200

    updated the window size to be smaller

--- neovim/.config/nvim/lua/plugins/oil.lua
+++ neovim/.config/nvim/lua/plugins/oil.lua
@@ -9,6 +9,15 @@ return {
 			default_file_explorer = false,
 
 			vim.keymap.set("n", "<leader>e", require("oil").toggle_float),
+
+			skip_confirm_for_simple_edits = false,
+
+			-- floating window config
+			float = {
+				max_width = 0.6,
+				max_height = 0.7,
+				border = "rounded",
+			},
 		})
 	end,
 }
