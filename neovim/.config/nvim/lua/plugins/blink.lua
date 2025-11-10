11f429bde8d5310479a7288ce29ace8fe8c710c3
Kevin Christ <christkevin50@gmail.com>
2025-11-10 11:40:31 +0100

    adding dql support

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,22 +1,26 @@
 return {
-    "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    version = "1.*",
-    opts = {
-        keymap = { preset = "default" },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+	opts = {
+		keymap = { preset = "default" },
 
-        appearance = {
-            nerd_font_variant = "mono",
-        },
+		appearance = { nerd_font_variant = "mono" },
 
-        completion = { documentation = { auto_show = false } },
+		completion = { documentation = { auto_show = false } },
 
-        sources = {
-            default = { "lsp", "path", "snippets", "buffer" },
-        },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+			per_filetype = {
+				sql = { "dadbod", "buffer" },
+			},
+		},
 
-        fuzzy = { implementation = "prefer_rust_with_warning" },
+		providers = {
+			dadbod = { module = "vim_dadbod_completion.blink" },
+		},
 
-    },
-    opts_extend = { "sources.default" },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
